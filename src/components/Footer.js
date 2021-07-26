edfb2576af29f02e6197aae284f57c3941b93928
Emily Morgenbesser <emilymorgenbesser@Emilys-MacBook-Pro.local>
2021-07-26 16:22:51 -0400

    revised representative search react

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+function Footer (){
+    return(
+        <p className="footer">Designed by Emily Morgenbesser</p>
+    )
+}
+
+export default Footer
---
+++ src/components/Representativesearch.js
@@ -0,0 +1,50 @@
+import { useState } from "react"
+import { APIKEY } from "../config.js"
+import RepresentativeSearchResults from "./RepresentativeSearchResults.js"
+
+function RepresentativeSearch() {
+    const [address, setAddress] = useState('')
+    const [offices, setOffices] = useState([])
+    const [officials, setOfficials] = useState([])
+    
+    const searchRepresentatives = () => {
+        console.log('searching...')
+        // fetch or civic info end point
+
+        fetch(`https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=${address}&key=${APIKEY}`)
+        .then(response => response.json())
+        .then(json => {
+            setOffices(json.offices)
+            setOfficials(json.officials)
+            return 
+        })
+        .catch(error => console.log(error))
+        // then store results in my state
+
+    }
+    
+    //Representativesearch.js:11 GET https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=6503%20n%20military%20trail%20boca%20raton%20fl&key=AIzaSyAEhz_fsejkroNfQw1hctZSP7Ib1PcWmdU
+    
+    return (
+    <div className="search-container">
+        <input
+        name="representativesearch"
+        type="text"
+        className="search-bar"
+        value={address}
+        onChange={e => setAddress(e.target.value)}
+        />
+        <div>
+        <button 
+        className="search-btn"
+        onClick={() => searchRepresentatives()}
+        >
+            Submit
+        </button>
+        </div>
+        <RepresentativeSearchResults offices={offices} officials={officials} />
+    </div>
+    )
+}
+
+export default RepresentativeSearch
